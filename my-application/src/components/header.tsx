5a8ae292e5dfff970f5b6bc962fc3795899fcfce
Pankaj Jain <47389183+pankajpj07@users.noreply.github.com>
2023-05-16 20:00:18 +0000

    changed header and footer

--- my-application/src/components/header.tsx
+++ my-application/src/components/header.tsx
@@ -1,42 +1,47 @@
-import { useState } from 'react';
-import { logoPath } from '@/constants/constants';
-import Link from 'next/link';
+import { useState } from 'react'
+import { logoPath } from '@/constants/constants'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+    setIsMenuOpen(!isMenuOpen)
+  }
 
   return (
-    <header className="bg-white py-6 px-6 shadow sticky top-0 w-full z-[1]">
+    <header className="bg-white py-6 px-6 shadow sticky top-0 w-full z-1">
       <div className="flex items-center justify-between">
         <div>
-          <Link href="/" passHref legacyBehavior>
-            <a>
-              <img src={logoPath} alt="Logo" className="w-8 h-8" />
-            </a>
+          <Link href="/">
+            <span>
+              <Image src={logoPath} alt="Logo" className="w-8 h-8" />
+            </span>
           </Link>
         </div>
-        <div className='hidden md:block'>
-        <div className={`flex items-center space-x-6  md:block`}>
-          <Link href="/" passHref legacyBehavior>
-            <a className="text-black hover:text-gray-500">Home</a>
-          </Link>
-          <Link href="/job-support" passHref legacyBehavior>
-            <a className="text-black hover:text-gray-500">Job Support</a>
-          </Link>
-          <Link href="/join-us" passHref legacyBehavior>
-            <a className="text-black hover:text-gray-500">Join Us</a>
-          </Link>
-          <Link href="/contact-us" passHref legacyBehavior>
-            <a className="text-black hover:text-gray-500">Contact Us</a>
-          </Link>
-          <Link href="/book-demo" passHref legacyBehavior>
-            <a className="px-4 py-2 text-white bg-black rounded-md hover:bg-gray-800">Book a Demo</a>
-          </Link>
-        </div>
+        <div className="hidden md:block">
+          <div className={`flex items-center space-x-6  md:block`}>
+            <Link href="/">
+              <span className="text-black hover:text-gray-500">Home</span>
+            </Link>
+            <Link href="/job-support">
+              <span className="text-black hover:text-gray-500">
+                Job Support
+              </span>
+            </Link>
+            <Link href="/join-us">
+              <span className="text-black hover:text-gray-500">Join Us</span>
+            </Link>
+            <Link href="/contact-us">
+              <span className="text-black hover:text-gray-500">Contact Us</span>
+            </Link>
+            <Link href="/book-demo">
+              <span className="px-4 py-2 text-white bg-black rounded-md hover:bg-gray-800">
+                Book a Demo
+              </span>
+            </Link>
+          </div>
         </div>
         <div className="sm:hidden">
           <button
@@ -80,27 +85,34 @@ const Header = () => {
       </div>
       {/* Collapsible Menu */}
       <div className={`mt-4 sm:hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
-        <Link href="/" passHref legacyBehavior>
-          <a className="block py-2 text-black hover:text-gray-500">Home</a>
+        <Link href="/">
+          <span className="block py-2 text-black hover:text-gray-500">
+            Home
+          </span>
         </Link>
-        <Link href="/job-support" passHref legacyBehavior>
-          <a className="block py-2 text-black hover:text-gray-500">Job Support</a>
+        <Link href="/job-support">
+          <span className="block py-2 text-black hover:text-gray-500">
+            Job Support
+          </span>
         </Link>
-        <Link href="/join-us" passHref legacyBehavior>
-          <a className="block py-2 text-black hover:text-gray-500">Join Us</a>
+        <Link href="/join-us">
+          <span className="block py-2 text-black hover:text-gray-500">
+            Join Us
+          </span>
         </Link>
-        <Link href="/contact-us" passHref legacyBehavior>
-          <a className="block py-2 text-black hover:text-gray-500">Contact Us</a>
+        <Link href="/contact-us">
+          <span className="block py-2 text-black hover:text-gray-500">
+            Contact Us
+          </span>
         </Link>
-        <Link href="/book-demo" passHref legacyBehavior>
-          <a className="block py-2 px-4 mt-2 text-white bg-black rounded-md hover:bg-gray-800">
+        <Link href="/book-demo">
+          <span className="block py-2 px-4 mt-2 text-white bg-black rounded-md hover:bg-gray-800">
             Book a Demo
-          </a>
+          </span>
         </Link>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
-
+export default Header
